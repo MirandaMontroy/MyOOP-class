b12143684410f572582965eb925fd820ed9ea755
Miranda Montroy <Miranda_Montroy1@baylor.edu>
2021-02-10 14:54:31 -0600

    changes made to student class

---
+++ Student class demo.py
@@ -0,0 +1,10 @@
+import StudentClass as s
+
+
+def main():
+    john_doe = s.Student(12345, "John Doe", "9-27-1999", "S")
+
+    john_doe.when_register()
+
+
+main()
--- StudentClass.py
+++ StudentClass.py
@@ -1,6 +1,18 @@
 class Student:
-    def __init__(self):
-        self.id
-        self.name
-        self.dob
-        self.classification
+    def __init__(self, idnum, name, dob, classification):
+        self.idnum = idnum
+        self.name = name
+        self.dob = dob
+        self.classification = classification
+
+    def when_register(self):
+        if self.classification == "Sr":
+            print("Seniors register 11/1-11/3")
+        elif self.classification == "Jr":
+            print("Juniors register 11/4-11/6")
+        elif self.classification == "S":
+            print("Sophomores register 11/7-11/9")
+        elif self.classification == "F":
+            print("Freshman register 11/10-11/12")
+        else:
+            print("Invalid classification")
