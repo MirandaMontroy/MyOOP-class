7381f06b8199fd8875f01ab7a0d385f3cf2a12db
Miranda Montroy <Miranda_Montroy1@baylor.edu>
2021-02-10 13:00:22 -0600

    attempt at in class example

---
+++ Insect flight.py
@@ -0,0 +1,12 @@
+import InsectClass as b
+
+
+def main():
+    my_bug = b.Insect()
+
+    my_bug.flight_distance
+
+    print("This is how far the bug can fly: ", my_bug.get_distance)
+
+
+main()
---
+++ InsectClass.py
@@ -0,0 +1,14 @@
+import random
+
+
+class Insect:
+    def __init__(self):
+        self.wings = 2
+        self.legs = 4
+        self.distance = 0
+
+    def flight_distance(self):
+        self.distance= random.randint(1, 10)
+
+    def get_distance(self):
+        return self.distance
